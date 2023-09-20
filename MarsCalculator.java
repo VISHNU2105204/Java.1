336dfe5fa9107e871977679bfef8b38212a26fa4
VISHNU <120297452+VISHNU2105204@users.noreply.github.com>
2023-09-20 14:58:43 +0530

    Update MarsCalculator.java
    none

--- MarsCalculator.java
+++ MarsCalculator.java
@@ -1,4 +1,4 @@
-/*Write a program for Weight on Mars calculator is one of the fun and
+/*Practical6 : Write a program for Weight on Mars calculator is one of the fun and
 simple programs of Java to implement as a beginner. This program calculates a
 person’s weight on Mars based on their weight on Earth. All you need to do is ask
 the user for their weight on Earth and then print out their weight on Mars. In order
@@ -7,13 +7,21 @@ means that if someone weighs 100 pounds on Earth, they would weigh 38 pounds
 on Mars.*/
 
 import java.util.Scanner;
+
 public class Practical6 {
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter your Weight on Earth (in pound): ");
-double earthWeight = sc.nextDouble();
-double marsWeight = earthWeight * 0.38;
-System.out.println("Your weight on mars would be: " + marsWeight
-+ " pounds");
-}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter your Weight on Earth (in pound): ");
+		
+		double earthWeight = sc.nextDouble();
+		
+		double marsWeight = earthWeight * 0.38;
+		
+		System.out.println("Your weight on mars would be: " + marsWeight + " pounds");
+		
+	}
+
 }
